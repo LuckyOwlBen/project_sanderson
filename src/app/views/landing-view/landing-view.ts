531e1886aa0549dc7b4933c60be0e1836ec90861
ben malmberg <benmalmberg2@gmail.com>
2025-12-08 14:53:19 -0700

    spicier landing page

--- src/app/views/landing-view/landing-view.ts
+++ src/app/views/landing-view/landing-view.ts
@@ -1,20 +1,31 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-landing-view',
-  imports: [],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatIconModule
+  ],
   templateUrl: './landing-view.html',
   styleUrl: './landing-view.scss',
 })
 export class LandingView {
 
   constructor(private router: Router) {}
+  
   newCharacter() {
     this.router.navigateByUrl('/character-creator-view')
   }
 
   loadCharacter() {
     this.router.navigateByUrl('/load-character')
+  }
+
+  viewSheet() {
+    this.router.navigateByUrl('/character-sheet')
   }
 }
