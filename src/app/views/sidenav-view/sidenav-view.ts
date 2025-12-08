9d78342fcb389a4918ff0099e6ec77d89128be65
ben malmberg <benmalmberg2@gmail.com>
2025-12-08 13:50:52 -0700

    added sidebar content

--- src/app/views/sidenav-view/sidenav-view.ts
+++ src/app/views/sidenav-view/sidenav-view.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-sidenav-view',
   imports: [
-    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
     MatDividerModule,
+    MatListModule,
   ],
   templateUrl: './sidenav-view.html',
   styleUrl: './sidenav-view.scss',
 })
 export class SidenavView {
+  constructor(private router: Router) {}
 
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
 }
